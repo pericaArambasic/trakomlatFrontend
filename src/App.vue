8ecd4ad4ceaa6b314974c0d0da1a5064768a6d36
Perica <=>
2021-02-19 17:18:49 +0100

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <display></display>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Display from "@/components/Display";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Display,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Display.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <div>
+
+    <v-container>
+
+      <template>
+
+        <v-card
+            class="mx-auto my-12"
+            width="560"
+            height="315"
+        >
+          <template slot="progress">
+          </template>
+
+          <iframe width="560" height="315" src="https://www.youtube.com/watch?v=8dhSq9mD6TI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
+          <v-card-title>Cafe Badilico</v-card-title>
+
+
+          <v-divider class="mx-4"></v-divider>
+
+          <v-card-text>
+            <v-chip-group
+                v-model="selection"
+                active-class="deep-purple accent-4 white--text"
+                column
+            >
+              <v-chip>9:00PM</v-chip>
+            </v-chip-group>
+          </v-card-text>
+
+        </v-card>
+      </template>
+
+    </v-container>
+
+  </div>
+
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Display",
+  data: () => ({
+    info: null
+  }),
+  mounted() {
+    axios
+     .get('http://trakomlat/api/music')
+    .then(response => (this.info = response.data))
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
