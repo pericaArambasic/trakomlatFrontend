86d219a7a692b7ad9ff59e984df0c7cd5b7defb7
Perica <=>
2021-02-24 17:24:16 +0100

    working around submitLink function

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,24 +1,25 @@
 <template>
 
     <v-container >
-
-        <div class="text-center" v-for="item in info" :key="item.id">
-
+        <!-- div that contains ytb link from database-->
+        <div v-for="item in info" :key="item.id">
           <iframe width="560" height="315" :src="'https://www.youtube.com/embed/' + item.link"
                   frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           <h4>{{ item.name }}</h4>
           <p>{{item.link}}</p>
 
+          <!--form that deletes current link-->
+          <v-form>
+            <p>http://trakomlat/api/music/{{item.id}}</p>
+              <v-btn type="submit" @click="deleteLink(item.id)">DELETE {{item.id}}</v-btn>
+          </v-form>
         </div>
 
+      <!-- form that submits new link to a database-->
       <div>
-
         <v-form>
-
           <v-text-field v-model="form.name" label="Name of the song" required></v-text-field>
-
           <v-text-field v-model="form.link" label="Link" required></v-text-field>
-
           <v-btn type="submit" @click="submitLink">SUBMIT</v-btn>
         </v-form>
         <p>{{form}}</p>
@@ -49,6 +50,10 @@ export default {
   methods: {
     submitLink: function ()
     {
+      // eslint-disable-next-line no-unused-vars
+      let proper = null;
+      proper = this.form.link.split("=");
+      proper = this.form.link[0];
       axios
           .post('http://trakomlat/api/music', this.form)
       .then(function (response) {
@@ -57,8 +62,20 @@ export default {
       .catch(function (error) {
         console.log(error)
       })
+    },
 
+    deleteLink: function (id)
+    {
+      axios
+      .delete('http://trakomlat/api/music/'+ id)
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
     }
+
     }
   }
 
